622ebad21ea02a7bfcfad7286cb9c6c66c766ab4
minhaz <minhaz.taher@gmail.com>
2020-03-18 12:04:11 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./pages/Error";
+import SingleCocktail from "./pages/SingleCocktail";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./App.css";
-import Header from "./component/Header/Header";
+import Navbar from "./conponents/Navbar";
 
 function App() {
   return (
     <>
-      <Header></Header>
+      <Navbar></Navbar>
+      <Router>
+        <Switch>
+          <Route path="/About">
+            <About />
+          </Route>
+          <Route path="/Home">
+            <Home />
+          </Route>
+          <Route path="/cocktail/:id">
+            <SingleCocktail />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
