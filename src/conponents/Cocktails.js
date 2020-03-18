3d66cafd9788badfcc1f8aa3c97a4539d1cc5b91
minhaz <minhaz.taher@gmail.com>
2020-03-18 16:01:25 +0600

    singlr page left

--- src/conponents/Cocktails.js
+++ src/conponents/Cocktails.js
@@ -1,7 +1,19 @@
 import React from "react";
-
-const Cocktails = props => {
-  return <div>Cocktails</div>;
-};
-
-export default Cocktails;
+import { Link } from "react-router-dom";
+export default function Cocktail({ image, name, id, info, glass }) {
+  return (
+    <div className="coc-con left" >
+      <div className="coc-img">
+        <img src={image} alt={name} />
+      </div>
+      <div className="cocktail-footer">
+        <h3> {name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className="btn btn-primary btn-details">
+          details
+        </Link>
+      </div>
+    </div>
+  );
+}
